16c4f557b5cd5c8a63979766e19efa8dfac923ca
zachhuang4026 <zachhuang4026@gmail.com>
2022-03-01 20:29:33 -0600

    add topBarButton functions

---
+++ My Chef-SwiftUI/My Chef-SwiftUI/GroceryListView.swift
@@ -0,0 +1,20 @@
+//
+//  GroceryListView.swift
+//  My Chef-SwiftUI
+//
+//  Created by Zach Huang on 2022/3/1.
+//
+
+import SwiftUI
+
+struct GroceryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroceryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroceryListView()
+    }
+}
--- My Chef-SwiftUI/My Chef-SwiftUI/PlansView.swift
+++ My Chef-SwiftUI/My Chef-SwiftUI/PlansView.swift
@@ -7,115 +7,133 @@
 
 import SwiftUI
 
-
 struct PlansView: View {
     var days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
     var recipes: [Recipe] = []
     
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack {
-                    Spacer()
-                    Button("Generate \n Grocery List") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    .padding()
-                    .background(Color("buttonColor"))
-                    .foregroundColor(Color("textColor"))
-                    .lineLimit(2)
-                    .cornerRadius(15.0)
+        NavigationView {
+            ScrollView {
+                VStack {
+                    topBarButton()
                     
-                    Spacer()
-                    
-                    Button("View \n My Fridge") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    ForEach(0..<7) {
+                        Divider()
+                        Text("\(days[$0])")
+                            .foregroundColor(Color("textColor"))
+                            .font(.title)
+                        Text("03/02")
+                        Divider()
+                        mealsScrollView()
                     }
-                    .padding()
-                    .background(Color("buttonColor"))
-                    .foregroundColor(Color("textColor"))
-                    .lineLimit(2)
-                    .cornerRadius(15.0)
-                    
-                    Spacer()
-                }
-                
-                ForEach(0..<7) {
-                    Divider()
-                    Text("\(days[$0])")
-                        .foregroundColor(Color("textColor"))
-                        .font(.title)
-                    Text("03/02")
-                    Divider()
-                    ScrollView(.horizontal) {
-                        HStack {
-                            VStack {
-                                Text("Breakfast")
-                                Text("Avocado Toast")
-                                Text("Easy | 10 mins")
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                                AsyncImage(url: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")) { image in
-                                                           image
-                                                            .resizable()
-                                                            .scaledToFill()
-                                            } placeholder: {
-                                                ProgressView()
-                                            }
-                                            .frame(width: 100, height: 100)
-                                            .background(Color.gray)
-                                            .cornerRadius(15.0)
-                            }
-                            VStack {
-                                Text("Lunch")
-                                Text("Pasta")
-                                Text("Easy | 10 mins")
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                                AsyncImage(url: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")) { image in
-                                                           image
-                                                            .resizable()
-                                                            .scaledToFill()
-                                            } placeholder: {
-                                                ProgressView()
-                                            }
-                                            .frame(width: 100, height: 100)
-                                            .background(Color.gray)
-                                            .cornerRadius(15.0)
-                            }
-                            VStack {
-                                Text("Dinner")
-                                Text("Pasta")
-                                Text("Easy | 10 mins")
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                                AsyncImage(url: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")) { image in
-                                                           image
-                                                            .resizable()
-                                                            .scaledToFill()
-                                            } placeholder: {
-                                                ProgressView()
-                                            }
-                                            .frame(width: 100, height: 100)
-                                            .background(Color.gray)
-                                            .cornerRadius(15.0)
-                            }
-                        }
-                        
-                        
-                    }
-                    .padding()
-                        
                     
                 }
-                
             }
+            .navigationTitle("Meals for the week")
         }
+        
     }
 }
 
 struct PlansView_Previews: PreviewProvider {
     static var previews: some View {
         PlansView(recipes: recipesTestData)
+    }
+}
+
+struct topBarButton: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Button(action: {
+            }) {
+                NavigationLink(destination: GroceryListView()) {
+                Text("Generate \n Grocery List")
+                }
+            }
+            .padding()
+            .background(Color("buttonColor"))
+            .foregroundColor(Color("textColor"))
+            .lineLimit(2)
+            .cornerRadius(15.0)
+            
+            Spacer()
+            Button(action: {
+            }) {
+                NavigationLink(destination: GroceryListView()) {
+                Text("View \n My Fridge")
+                }
+            }
+            .padding()
+            .background(Color("buttonColor"))
+            .foregroundColor(Color("textColor"))
+            .lineLimit(2)
+            .cornerRadius(15.0)
+            
+            Spacer()
+        }
+    }
+}
+
+struct mealsScrollView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                VStack {
+                    Text("Breakfast")
+                    Text("Avocado Toast")
+                    Text("Easy | 10 mins")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                    AsyncImage(url: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 100, height: 100)
+                    .background(Color.gray)
+                    .cornerRadius(15.0)
+                }
+                VStack {
+                    Text("Lunch")
+                    Text("Pasta")
+                    Text("Easy | 10 mins")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                    AsyncImage(url: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 100, height: 100)
+                    .background(Color.gray)
+                    .cornerRadius(15.0)
+                }
+                VStack {
+                    Text("Dinner")
+                    Text("Pasta")
+                    Text("Easy | 10 mins")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                    AsyncImage(url: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 100, height: 100)
+                    .background(Color.gray)
+                    .cornerRadius(15.0)
+                }
+            }
+            
+            
+        }
+        .padding()
     }
 }
